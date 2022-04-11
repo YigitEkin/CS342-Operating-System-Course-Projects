e4458cf68fa65026e6286ef0c303e44961a43ee5
YigitEkin <yigit.ekin@ug.bilkent.edu.tr>
2022-04-11 21:25:49 +0300

    initial commit for project 3

---
+++ project3/dma.c
@@ -0,0 +1,23 @@
+#include "dma.h"
+
+int   dma_init (int m) {
+
+}
+void *dma_alloc (int size) {
+
+}
+void  dma_free (void *p) {
+
+}
+void  dma_print_page(int pno){ 
+
+}
+void  dma_print_bitmap(){ 
+
+}
+void  dma_print_blocks(){ 
+
+}
+int   dma_give_intfrag(){ 
+
+}
