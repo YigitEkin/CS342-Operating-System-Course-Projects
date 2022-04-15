f18c05c8ead93d1711ee57543233f200e085a9eb
YigitEkin <yigit.ekin@ug.bilkent.edu.tr>
2022-04-16 02:56:26 +0300

    implemented dma_free and added thread safety

--- project3/dma.c
+++ project3/dma.c
@@ -65,7 +65,6 @@ size_t find_free_space(int asked_amount){
 } 
     
 int dma_init (int m) {
-    //TODO: fix size
     pthread_mutex_init(&segment_mutex, NULL);
     pthread_mutex_init(&internal_fragmentation_mutex, NULL);
     segment = mmap(0, pow(2,m), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
@@ -76,62 +75,93 @@ int dma_init (int m) {
     size = pow(2,m);
     bitmap_size = size / WORD_SIZE;
 
-    //! MAP BITMAP REGION TO BITMAP  
-    segment[0] = 0; segment[1] = 1; //as the bitmap is allocated, the first 2 bits will be 01
+    // MAP BITMAP REGION TO BITMAP  
+    segment[0] = 0; segment[1] = 1; 
     for (size_t i = 2; i < bitmap_size; i++)
     {
         segment[i] = 0;
     }
     
-    //!MAP SHARED AREA REGION TO BITMAP 
+    // MAP SHARED AREA REGION TO BITMAP 
     segment[bitmap_size] = 0; segment[bitmap_size + 1] = 1;
     for (size_t i = 0; i < 30; i++)
     {
         segment[bitmap_size + 2 + i] = 0;
     }
 
-    //! MAP ALLOCABLE AREA REGION TO BITMAP 
+    // MAP ALLOCABLE AREA REGION TO BITMAP 
     for (size_t i = bitmap_size + 32; i < bitmap_size; i++)
     {
         segment[i] = 1;
     }
-    
-    //add bitmap, reserved area and initiliaze the pages
     internal_fragmentation_size = 0;
     return 0;
 }
 
 void *dma_alloc (int size) {
     int size_to_allocate = calculate_closest_to16(size);
-    int bitmap_index = find_free_space(size_to_allocate);
-    segment[bitmap_index] = 0;
-    segment[bitmap_index + 1] = 1;
+    pthread_mutex_lock(&segment_mutex);
+    int sharedmem_bitmap_index = find_free_space(size_to_allocate);
+    if (sharedmem_bitmap_index == NULL) {
+        pthread_mutex_unlock(&segment_mutex);
+        return NULL;
+    }
+    segment[sharedmem_bitmap_index] = 0;
+    segment[sharedmem_bitmap_index + 1] = 1;
     for (size_t i = 2; i < size_to_allocate/WORD_SIZE; i++)
     {
-        segment[bitmap_index + i] = 0;
+        segment[sharedmem_bitmap_index + i] = 0;
     }
-    
-    //TODO: return address from allocated space
     internal_fragmentation_size += (size_to_allocate - size);
-    //search for the first free page
-    //update the bitmap
-    //update internal fragmentation size
+
+    //calculate the amount of displacement in shared memory region
+    size_t sharedmem_bitmap_start_index = BITMAP_RESERVED_AREA_SIZE + bitmap_size/WORD_SIZE;
+    size_t sharedmem_displacement = sharedmem_bitmap_index - sharedmem_bitmap_start_index;
+    void* return_val =  segment + sharedmem_displacement + bitmap_size + 32;
+    pthread_mutex_unlock(&segment_mutex);
+    return return_val;
 }
+
+//! There is a bug when two allocated blocks are located next to each other
 void  dma_free (void *p) {
-    //search for the first freeable page
-    //update the bitmap
-    //update internal fragmentation size
+    size_t sharedmem_start_index = bitmap_size + 32;
+    pthread_mutex_lock(&segment_mutex);
+    size_t sharedmem_displacement = (int*)p - (segment + sharedmem_start_index);
+
+    size_t sharedmem_bitmap_start_index = BITMAP_RESERVED_AREA_SIZE + bitmap_size/WORD_SIZE;
+    size_t starting_index_to_free_in_bitmap = sharedmem_bitmap_start_index + sharedmem_displacement;
+
+    segment[starting_index_to_free_in_bitmap++] = 1; //made 01 to 11
+    ++starting_index_to_free_in_bitmap; //skipped 1 index in 01
+
+    //made other 0 values to 1
+    while (segment[starting_index_to_free_in_bitmap] == 0) {
+        segment[starting_index_to_free_in_bitmap] = 1;
+        ++starting_index_to_free_in_bitmap;
+    }
+    pthread_mutex_unlock(&segment_mutex);
 }
+
+//! What amount of data should be printed needs to be fixed
 void  dma_print_page(int pno){ 
     //print the page pno
     size_t page_starting_index = pno * PAGE_SIZE;
-
-    //TODO: create 64 hexadecimal block to print
     size_t index = 0;
+    //create a string to print
+    char str[64];
+    //while index < 64 append segment[index] to str
+    while (index < 64 && index < size) {
+        pthread_mutex_lock(&segment_mutex);
+        str[index] = segment[page_starting_index + index];
+        index++;
+        pthread_mutex_unlock(&segment_mutex);
+    }
+    bin_to_hex(str);
 }
+
 void  dma_print_bitmap(){ 
     pthread_mutex_lock(&segment_mutex);
-    for (size_t i = 0; i < size; i++)
+    for (size_t i = 0; i < bitmap_size; i++)
     {
         if ((i % 64 == 0) &&  (i != 0)) {
             printf("\n");        
@@ -142,8 +172,8 @@ void  dma_print_bitmap(){
     }
     pthread_mutex_unlock(&segment_mutex);
 }
-void  dma_print_blocks(){ 
 
+void  dma_print_blocks(){ 
 }
 
 int dma_give_intfrag(){ 
